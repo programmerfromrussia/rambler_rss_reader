20a1df397640b4bce547b22d0256eaab0a0fe59c
programmerfromrussia <112801852+programmerfromrussia@users.noreply.github.com>
2023-10-19 01:18:35 +0300

    Adding header file for multiple use

---
+++ header.php
@@ -0,0 +1,15 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<header>
+    <div id="header-content">
+        <div class="nav">
+            <a href="index.php" class="nav-link"><i class="fas fa-home"></i> Главная</a>
+            <a href="bookmarks.php" class="nav-link"><i class="fas fa-bookmark"></i> Сохранённое</a>
+            <a href="index.php" class="nav-link site-title">Новости на сегодня</a>
+        </div>
+    </div>
+</header>
